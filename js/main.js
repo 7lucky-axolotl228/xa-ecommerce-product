97b87f01ed243e1c07fbcc90f9f72fc4df6c58d0
7lucky-axolotl228 <7error228@gmail.com>
2023-08-02 19:49:24 +0500

    Add active state to thubmnails img

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+/* Variables */
+const modifiers = {
+  imgThumbnailActive: 'img-showcase__thumbnail--active'
+};
+
+/* Functions */
+function deactivateThumbnailBtn () {
+  elsImgShowcaseThumbnailBtn.forEach(function (elButton) {
+    elButton.parentElement.classList.remove(modifiers.imgThumbnailActive);
+  });
+}
+
+
+/* Shopping cart modal */
 const elSiteHeaderCartLink = document.querySelector('.js-site-header-cart-link');
 const elSiteHeaderCartModal = document.querySelector('.site-header__cart-modal');
 
@@ -8,3 +22,19 @@ if (elSiteHeaderCartLink) {
     elSiteHeaderCartModal.classList.toggle('site-header__cart-modal--open');
   });
 }
+
+
+/* ImgShowcase */
+const elsImgThumbnail = document.querySelectorAll('.img-showcase__thumbnail');
+const elsImgShowcaseThumbnailBtn = document.querySelectorAll('.img-showcase__thumbnail-btn');
+
+
+elsImgShowcaseThumbnailBtn.forEach(function (elButton) {
+  elButton.addEventListener('click', function (evt) {
+    /* Remove active class from btn */
+    deactivateThumbnailBtn();
+
+    /* Add --active class to buttons */
+    elButton.closest('.img-showcase__thumbnail').classList.add(modifiers.imgThumbnailActive);
+  });
+});
